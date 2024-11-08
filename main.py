b2d2d315cee30aaf22897ce49ac17caed7384986
Feotull <vsarkov@gmail.com>
2024-11-08 12:50:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# "1st program"
+print (9 ** 0.5 * 5)
+# "2nd program"
+print (9.99 > 9.98 and 1000 != 1000.01)
+# "3rd program"
+print (2 * 2 + 2)
+print (2 * (2 + 2))
+print ((2 * 2 + 2) == (2 * (2 + 2)))
+print (6 == 8)
+# "4th program"
+print ('123.456')
+print (type('123.456'))
+print (float('123.456'))
+print (type (float('123.456')))
+print (float('123.456') * 10)
+print (int(float('123.456') * 10))
+print (type(int(float('123.456') * 10)))
+print (int(float('123.456') * 10 % 10))
