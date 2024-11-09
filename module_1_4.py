963f34890fd307280a5d5d61b8cfc65f8e0f0525
Feotull <vsarkov@gmail.com>
2024-11-09 17:57:50 +0300

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -2,4 +2,4 @@ Task_done = 12
 Hours_spent = 1.5
 Name_of_course = "Python"
 Estimated_time = Hours_spent/Task_done
-print (Name_of_course, Task_done, Hours_spent, Hours_spent/Task_done)
+print (Name_of_course, Task_done, Hours_spent, Estimated_time)
