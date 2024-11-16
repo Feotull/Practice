0254cefcedf77912c4f809b42f510c3b08660ccb
Feotull <vsarkov@gmail.com>
2024-11-16 11:46:54 +0300

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,5 +1,25 @@
-Task_done = 12
-Hours_spent = 1.5
-Name_of_course = "Python"
-Estimated_time = Hours_spent/Task_done
-print (Name_of_course, Task_done, Hours_spent, Estimated_time)
+# 1. В проекте, где вы решаете домашние задания, создайте модуль 'module_1_4.py' и напишите весь код в нём.
+#
+# 2. Организуйте программу:
+#
+# Создайте переменную my_string и присвойте ей значение строки с произвольным текстом (функция input()).
+# Вывести количество символов введённого текста
+# 3. Работа с методами строк:
+#
+# Используя методы строк, выполните следующие действия:
+#
+# Выведите строку my_string в верхнем регистре.
+# Выведите строку my_string в нижнем регистре.
+# Измените строку my_string, удалив все пробелы.
+# Выведите первый символ строки my_string.
+# Выведите последний символ строки my_string.
+
+my_string = input ('Введите произвольный текст: ')
+len_mystring = len (my_string)
+print (type(len_mystring))
+print (f"Количество символов введенного текста: {len_mystring}")
+print (my_string.upper())
+print (my_string.lower())
+print (my_string.replace (" ", ""))
+print(my_string[0])
+print(my_string[-1])
